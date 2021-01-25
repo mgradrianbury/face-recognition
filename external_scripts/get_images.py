c1ff6f159459afed227b674d694eae1244626afb
Adrian Bury <adrian.jakub.bury@gmail.com>
2021-01-25 21:34:27 +0100

    Remove old files

--- external_scripts/get_images.py
+++ external_scripts/get_images.py
@@ -3,18 +3,18 @@ from shutil import copyfile
 from os import path
 import pandas as pd
 
-data = pd.read_csv('/home/adrian/Downloads/zdjęcia-twarzy/CelebA/Anno/identity_CelebA.txt', header=None, sep=" ")
+data = pd.read_csv('/home/adrian/MyCurse/master_thesis/faces/CelebA/Anno/identity_CelebA.txt', header=None, sep=" ")
 ids = data[1].unique()
 first_fifty = ids[:50]
-images_path = "/home/adrian/Downloads/zdjęcia-twarzy/CelebA/Img/img_align_celeba"
-image_temp = "/home/adrian/Downloads/zdjęcia-twarzy/CelebA/Img/temp"
+images_path = "/home/adrian/MyCurse/master_thesis/faces/CelebA/Img/img_align_celeba"
+image_temp = "/home/adrian/MyCurse/master_thesis/faces/CelebA/Img/temp"
 
 for person_id in first_fifty:
     images_for_peron = data.loc[data[1] == person_id][0].array
-    images_for_tests = [images_for_peron[-1]]
-    images_for_training = images_for_peron[:-1]
+    images_for_tests = images_for_peron[10:12]
+    images_for_training = images_for_peron[0:10]
 
-    for file_name in images_for_peron:
+    for file_name in images_for_training:
         full_path = path.join(images_path, file_name)
         os.makedirs(
             path.join(image_temp, 'train', str(person_id)),
